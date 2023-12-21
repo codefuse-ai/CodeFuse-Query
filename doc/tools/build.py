050e89648e8c400900792d9e49e367aff825a52e
恕山 <lihaokun.lhk@antgroup.com>
2023-12-21 18:06:31 +0800

    update coref api builder process in ci

--- doc/tools/build.py
+++ doc/tools/build.py
@@ -1,15 +1,21 @@
 import subprocess
-import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python this_file.py godel_script_executable_path")
-    exit(-1)
-
-godel_script_executable = sys.argv[1]
-
+print("Download Sparrow CLI")
+subprocess.run([
+    "curl",
+    "-L",
+    "https://github.com/codefuse-ai/CodeFuse-Query/releases/download/2.0.2/sparrow-cli-2.0.2.linux.tar.gz",
+    "-o",
+    "sparrow-cli.tar.gz"
+])
+subprocess.run([
+    "tar",
+    "-xvzf",
+    "sparrow-cli-2.0.2.linux.tar.gz"
+])
 print("Copy ../assets into ./doc/assets")
 subprocess.run(["cp", "-r", "../assets", "./"])
 print("Concat coref library from ../language into ./.coref-api-build")
 subprocess.run(["python3", "tools/generate_coref_library.py", "../language"])
 print("Generate markdown documents into ./godel-api")
-subprocess.run(["python3", "tools/generate_markdown.py", godel_script_executable])
+subprocess.run(["python3", "tools/generate_markdown.py", "./sparrow-cli/godel-script/usr/bin/godel"])
